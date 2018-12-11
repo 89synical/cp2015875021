de086513c636d81ef1028cc20308b2d1c5269e17
2015875021 <bright_gate@hanmail.net>
2018-12-11 16:41:31 +0900

    20181211

--- 2015875021윤홍철/20181211/LinkedList.c
+++ 2015875021윤홍철/20181211/LinkedList.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node{
+typedef struct node {
 	int score;
 	struct node* pNext;
 } Node;
+typedef struct linkedlist {
+	Node* pHead;
+	Node* pTail;
+	int nNode;
+} LinkedList;
 //함수 printLL()
 //입력 : 연결리스트
 //출력 : 없음
 //부수효과 : 각 연결리스트의 성적을 순서대로 출력
-void printLL(Node* ptr) {
+void printLL(LinkedList* pList) {
+	Node* ptr = pList->pHead;
 	while (ptr != NULL) {
 		printf("score=%d\n", ptr->score);
 		ptr = ptr->pNext;
@@ -39,9 +45,12 @@ Node* createNode(int input) {
 //입력 : 연결리스트, 새 노드
 //출력 : 없음
 //부수효과 : 연결리스트 헤드에 새 노드를 추가
-void addHead(Node** ppHead, Node* pNewNode) {
-	pNewNode->pNext = *ppHead;
-	*ppHead = pNewNode;
+void addHead(LinkedList* pList, Node* pNewNode) {
+	if (pList->pHead == NULL)
+		pList->pTail = pNewNode;
+	pNewNode->pNext = pList->pHead;
+	pList->pHead = pNewNode;
+	(pList->nNode)++;
 }
 //함수 returnTail()
 //입력 : 연결리스트(의 헤드노드 포인터)
@@ -61,31 +70,21 @@ Node* returnTail(Node* ptr) {
 //출력 : 없음
 //부수효과 : 추가하려는 노드가 연결리스트의 tail로 추가됨
 void addTail(Node* pHead, Node* pNode) {
-	if (pHead == NULL){
+	if (pHead == NULL) {
 		printf("error can't add a tail to empty linked list \n");
-	return;
+		return;
 	}
 	Node* pTail = returnTail(pHead);
 	pTail->pNext = pNode;
 }
 int main() {
-	Node* pHead = NULL, *ptr;//헤드노드의 포인터, NULL이면 연결리스트가 없다는 의미(로 약속하자)
-//	Node a, b;
-//	a.score = '10';
-//	b.score = '20';
-//	a.pNext = &b;
-//	b.pNext = NULL;
-//	pHead = &a;
-	pHead = createNode(11);
-	ptr = createNode(20);
-//	addHead(&pHead, ptr);
-	addTail(pHead, ptr);
-	printLL(pHead);
-//	double test = 10 / 4; // 1. (10/4)->2를 반환, 2. 정수형 2를 double형 2.0으로 변환
-//	printf("test = %f\n", test);
-//	test = 10.0 / 4; //(10.0/4)->2.5를 반환
-//	printf("test = %f\n", test);
-//	test = ((double)10) / 4;//1.정수형(int) 10을 실수형(double) 10.0으로 변환
-//	printf("test = %f\n", test);
+	LinkedList *pList;
+	pList = (LinkedList*)malloc(sizeof(LinkedList));
+	pList->pHead = pList->pTail = NULL; pList->nNode = 0;
+	addHead(pList, createNode(10));
+	addHead(pList, createNode(20));
+	addHead(pList, createNode(30));
+	addHead(pList, createNode(40));
+	printLL(pList);
 	return 0;
 }
